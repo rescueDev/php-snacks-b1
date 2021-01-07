9bf3d87dd71f729b5ec5796d14ee25f9cc95d9ea
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-07 16:22:30 +0100

    esercizio rifatto semplificando

--- index.php
+++ index.php
@@ -7,39 +7,48 @@
     <link rel="stylesheet" href="main.css">
     <title>Hotels</title>
 
-    <!--
-         GOAL:
-    Partiamo da questo array di hotel.
-    https://www.codepile.net/pile/OEWY7Q1G
-    Stampare tutti i nostri hotel con tutti i dati disponibili.
-    Avremo un file PHP con il nostro "database" e un file con tutta la logica. -->
+    <! GOAL: Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G Stampare tutti i nostri hotel con tutti i dati disponibili. Avremo un file PHP con il nostro "database" e un file con tutta la logica. -->
 
-    <?php
-    require_once 'data.php';
+        <?php
+        require_once 'data.php';
 
 
-    #var_dump($hotels);
+        #var_dump($hotels);
 
-    ?>
+        ?>
 
-    </div>
+        </div>
 
 </head>
 
 <body>
 
+
     <?php
-    foreach ($hotels as $key => $l1) { ?>
+    foreach ($hotels as $hotel) { ?>
         <div class="container">
             <?php
-            foreach ($l1 as $key2 => $l2) { ?>
-
-                <h2><?php echo $key2 . ' => ' . $l2 ?></h2>
-
-            <?php   } ?>
+            echo "Nome: " . $hotel["name"];
+            echo "<br>";
+            echo "Descrizione: " . $hotel["description"];
+            echo "<br>";
+            echo "Parcheggio: ";
+            if ($hotel["parking"]) {
+                echo "Si";
+            } else {
+                echo "No";
+            };
+            echo "<br>";
+            echo "Voto: " . $hotel["vote"];
+            echo "<br>";
+            echo "Distanza dal centro: " . $hotel["distance_to_center"] . " km";
+            echo "<br>";
+            ?>
         </div>
-    <?php }
+    <?php
+    }
     ?>
+
 
 </body>
 
