5eeb5f652b2a6e8eee784801634c8e32014ffc5a
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-07 15:31:03 +0100

    esercizio finito

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
     <title>Hotels</title>
 
     <!--
@@ -19,17 +20,28 @@
 
     #var_dump($hotels);
 
-    foreach ($hotels as $key => $l1) {
-
-        foreach ($l1 as $key => $l2) { ?>
-
-            <h2><?php echo $l2 ?></h2>
-    <?php     }
-    }
-
-
-
     ?>
+
+
+
+    <?php
+    foreach ($hotels as $key => $l1) { ?>
+        <div class="container">
+            <?php
+            foreach ($l1 as $key2 => $l2) { ?>
+
+
+
+
+                <h2><?php echo $key2 . ' => ' . $l2 ?></h2>
+
+
+
+        <?php     }
+        }
+
+        ?>
+        </div>
 
 </head>
 
